b7ed7c1833da78ea060e81340907933c406c98bc
Marshall Bowers <elliott.codes@gmail.com>
2016-11-24 01:23:04 -0500

    Add Socket.IO server for receiving connections

--- packages/rotonde-core/src/index.js
+++ packages/rotonde-core/src/index.js
@@ -1,5 +1,10 @@
+import Server from 'socket.io';
 import server from './server';
 
-server(app => {
+const httpServer = server(app => {
   console.log(`Rotonde listening on ${app.get('host')}:${app.get('port')}`);
+  const io = Server(httpServer);
+  io.on('connection', socket => {
+    console.log('Received connection');
+  });
 });
