bb90814b60040ef1ebb5ceba46ab9d3086384c07
Marshall Bowers <elliott.codes@gmail.com>
2016-11-24 22:20:44 -0500

    Add some websocket debugging code

--- packages/rotonde-core/src/server.js
+++ packages/rotonde-core/src/server.js
@@ -10,6 +10,20 @@ export default callback => {
   server.set('host', process.env.HOST || '0.0.0.0');
   server.set('port', process.env.PORT || 3000);
   server.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
+  server.use('/', (req, res, next) => {
+    return res.send(`
+        <script src="/socket.io/socket.io.js"></script>
+        <script>
+          var socket = io(window.location.origin);
+          socket.on('connect', () => {
+            console.log('Connected')
+          });
+          socket.on('rotonde.acknowledge', payload => {
+            console.log('ack', payload)
+          });
+        </script>
+    `)
+  });
   return server.listen(
     server.get('port'),
     server.get('host'),
