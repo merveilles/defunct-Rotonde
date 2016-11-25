7516198c4220db2ea84f26b73ce616a2cae18641
Marshall Bowers <elliott.codes@gmail.com>
2016-11-25 17:12:31 -0500

    Install plugins when the server starts

--- packages/rotonde-core/src/index.js
+++ packages/rotonde-core/src/index.js
@@ -1,9 +1,15 @@
 import Server from 'socket.io';
 import server from './server';
 import { registerHandlers } from './handlers';
+import { installPlugins } from './plugins';
 
-const httpServer = server(app => {
+const httpServer = server(async app => {
   console.log(`Rotonde listening on ${app.get('host')}:${app.get('port')}`);
   const io = new Server(httpServer);
   registerHandlers(io);
+  try {
+    await installPlugins();
+  } catch (err) {
+    console.error(err);
+  }
 });
