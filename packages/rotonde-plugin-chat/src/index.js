cae09209c25b49d7b30f39660062c0730f818c46
Marshall Bowers <elliott.codes@gmail.com>
2016-11-26 00:06:14 -0500

    Set the type of rotonde-plugin-chat

--- packages/rotonde-plugin-chat/src/index.js
+++ packages/rotonde-plugin-chat/src/index.js
@@ -1,2 +1,5 @@
+import { PLUGIN_TYPE } from 'rotonde-plugin';
+
+export const TYPE = PLUGIN_TYPE.ISOMORPHIC;
 export { default as ChatClientPlugin } from './client';
 export { default as ChatServerPlugin } from './server';
