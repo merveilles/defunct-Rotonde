3bf447157fd65a7e5e73b47d387327b58d43ad36
Marshall Bowers <elliott.codes@gmail.com>
2016-11-25 20:10:02 -0500

    Add testing code for chat plugin

--- packages/rotonde-core/src/handlers/index.js
+++ packages/rotonde-core/src/handlers/index.js
@@ -4,4 +4,13 @@ import handleConnection from './handleConnection';
 export function registerHandlers(io) {
   const core = io.of(NAMESPACES.ROTONDE_CORE);
   core.on('connection', handleConnection);
+  if (process.env.NODE_ENV === 'development') {
+    const { ChatServerPlugin } = require('../../.rotonde_plugins/local/rotonde-plugin-chat');
+    const { ChatClientPlugin } = require('../../.rotonde_plugins/local/rotonde-plugin-chat');
+    const clientPlugin = new ChatClientPlugin('http://localhost:3000');
+    const plugin = new ChatServerPlugin(io);
+    setInterval(() => {
+      plugin.sendMessage('maxdeviant', 'blah');
+    }, 1000);
+  }
 }
