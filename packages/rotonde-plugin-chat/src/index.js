61d01ba576762e4b59faa24dd1b3b5a9357c02ac
Marshall Bowers <elliott.codes@gmail.com>
2016-11-26 12:07:58 -0500

    Remove extraneous exports

--- packages/rotonde-plugin-chat/src/index.js
+++ packages/rotonde-plugin-chat/src/index.js
@@ -2,11 +2,6 @@ import { PLUGIN_TYPE, PLUGIN_ENVIRONMENT } from 'rotonde-plugin';
 import ChatClientPlugin from './client';
 import ChatServerPlugin from './server';
 
-export {
-  ChatClientPlugin,
-  ChatServerPlugin
-};
-
 // Plugin export.
 export const TYPE = PLUGIN_TYPE.PLUGIN;
 export const ENVIRONMENT = PLUGIN_ENVIRONMENT.ISOMORPHIC;
