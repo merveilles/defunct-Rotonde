033c40048c0d50b2dc3db79346a84e2d44ffd807
Marshall Bowers <elliott.codes@gmail.com>
2016-11-27 16:41:08 -0500

    Remove implicit return

--- packages/rotonde-cli/src/client-adapters.js
+++ packages/rotonde-cli/src/client-adapters.js
@@ -2,5 +2,7 @@ import { loadClientAdapters } from './config';
 
 export default function registerClientAdapters(vorpal) {
   const clientAdapters = loadClientAdapters();
-  clientAdapters.forEach(clientAdapter => clientAdapter.default(vorpal));
+  clientAdapters.forEach(clientAdapter => {
+    clientAdapter.default(vorpal);
+  });
 }
